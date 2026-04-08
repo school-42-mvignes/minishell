f7d66bfb664eb1ff95bf000cc8a884fa2fd4fe5e
Matthieu Vignes <mvignes@f6r9s11.clusters.42paris.fr>
2026-04-08 12:52:37 +0200

    execution d'un simple commande fonction, mais pas l'exit status

--- srcs/execution/sep.c
+++ srcs/execution/sep.c
@@ -6,7 +6,7 @@
 /*   By: mvignes <mvignes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 18:40:34 by mvignes           #+#    #+#             */
-/*   Updated: 2026/03/31 13:50:29 by mvignes          ###   ########.fr       */
+/*   Updated: 2026/04/08 12:06:13 by mvignes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,24 @@ static int	exec_or(t_node *node, t_command *cmd1, t_command *cmd2)
 	(void)cmd1;
 	(void)cmd2;
 	printf("PAS ENCORE FAIT\n");
+	return (1);
+}
+
+static int	exec_simple_cmd(t_node *node)
+{
+	int		status;
+	int		last_status;
+	pid_t	finished_pid;
+	pid_t	pid;
+
+	pid = create_fork();
+	if (pid == 0)
+		exec_cmd(node, node->cmd->av, rebuild_env(&node->cmd->shell->env));
+	finished_pid = wait(&status);
+	if (finished_pid == node->last_pid)
+		last_status = status;
+	if (WIFEXITED(last_status))
+		return (WEXITSTATUS(last_status));
 	return (1);
 }
 
@@ -51,12 +69,16 @@ void	what_the_separator(t_node *node, t_shell *shell) // revoir la fonction car 
 	(void)shell;
 	int		exit;	//shell->exit_status
 
+	if (node->type == NODE_CMD)
+		// exit = what_the_buildin(node);
+		exit = exec_simple_cmd(node);
 	if (node->type == NODE_PIPE)
 		exit = exec_pip(node, node->left->cmd, node->right->cmd);
 	else if (node->type == NODE_AND)
 		exit = exec_and(node, node->left->cmd, node->right->cmd);
 	else if (node->type == NODE_OR)
 		exit = exec_or(node, node->left->cmd, node->right->cmd);
+	printf("exit = %i\n", shell->exit_status);
 	(void)exit;
 }
 
