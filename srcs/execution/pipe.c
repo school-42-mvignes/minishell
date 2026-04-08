3bac8f8e04d726a6c06a2dd4b0ef3faeb374e117
Matthieu Vignes <mvignes@f6r9s11.clusters.42paris.fr>
2026-04-08 18:03:14 +0200

    exec avec 1 pipe fini

--- srcs/execution/pipe.c
+++ srcs/execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: mvignes <mvignes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 16:54:22 by mvignes           #+#    #+#             */
-/*   Updated: 2026/04/08 11:48:42 by mvignes          ###   ########.fr       */
+/*   Updated: 2026/04/08 17:24:38 by mvignes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,25 +72,29 @@ t_redir	*search_last_fd_redir(t_redir *redir)
 void	exec_sec_cmd(t_node *node, t_command *cmd, int pipe[2])
 {
 	pid_t	pid;
+	int		fd;
 	t_redir	*redir;
 
 	pid = create_fork();
 	if (pid == 0)
 	{
-		printf("proubleme la\n");
-		printf("infile error = %s\n\n\n", cmd->redir->file);
-
 		close(pipe[1]);
 		redirect_fd(pipe[0], STDIN_FILENO);
-		redir = search_last_fd_redir(cmd->redir);
-		redir->file_fd = what_the_outfile(redir);
-		if (redir->file_fd == -1)
+		if (!cmd->redir)
+			fd = STDIN_FILENO;
+		else
 		{
-			close(pipe[0]);
-			// free_all(pipex);
-			// exit(EXIT_FAILURE);
+			redir = search_last_fd_redir(cmd->redir);
+			fd = what_the_outfile(redir);
+			if (fd == -1)
+			{
+				close(pipe[0]);
+				// free_all(pipex);
+				// exit(EXIT_FAILURE);
+			}
+			redirect_fd(fd, STDOUT_FILENO);
 		}
-		redirect_fd(redir->file_fd, STDOUT_FILENO);
+
 		exec_cmd(node, cmd->av, rebuild_env(&cmd->shell->env));
 	}
 	// last_child(node, cmd, pipe);
@@ -102,26 +106,29 @@ void	exec_sec_cmd(t_node *node, t_command *cmd, int pipe[2])
 void	exec_first_cmd(t_node *node, t_command *cmd, int pipe[2])
 {
 	pid_t	pid;
+	int		fd;
 
 	create_pipe(pipe);
 	pid = create_fork();
 	if (pid == 0)
 	{
-		printf("proubleme ici\n");
-		// printf("av[0] = %s\n\n\n", cmd->av[0]);
-		// printf("infile error = %d\n\n\n", cmd->redir->file_fd);
-		printf("infile error = %s\n\n\n", cmd->redir->file);
-		cmd->redir->file_fd = open_file_read(cmd->redir->file);
+
+		if (!cmd->redir)
+			fd = STDIN_FILENO;
+		else
+			fd = open_file_read(cmd->redir->file);
 		close(pipe[0]);
-		if (cmd->redir->file_fd == -1)
+		if (fd == -1)
 		{
 			// free_all(pipex);
 			close(pipe[1]);
+			printf("PROBLEME OUVERTURE FICHIER\n\n\n");
 			// exit(EXIT_FAILURE);
 		}
-		redirect_fd(cmd->redir->file_fd, STDIN_FILENO);
+		redirect_fd(fd, STDIN_FILENO);
 		redirect_fd(pipe[1], STDOUT_FILENO);
 		close(pipe[0]);
+
 		exec_cmd(node, cmd->av, rebuild_env(&cmd->shell->env));
 	}
 	close(pipe[1]);
