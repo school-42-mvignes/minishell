b05eeef63ccce522d203b895a08463ef0602dd7b
Matthieu Vignes <mvignes@f6r10s12.clusters.42paris.fr>
2026-04-14 14:43:32 +0200

    push main, le reste est casser

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmusquer <mmusquer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvignes <mvignes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 17:20:10 by mvignes           #+#    #+#             */
-/*   Updated: 2026/04/14 12:59:55 by mvignes          ###   ########.fr       */
+/*   Updated: 2026/04/14 14:21:55 by mvignes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,24 @@
 
 #include "../includes/minishell.h"
 
-// t_shell	*ft_shellnew(void)
-// {
-// 	t_shell	*element;
-// 	int		status;
-
-// 	status = 0;
-// 	element = malloc(sizeof(t_shell));
-// 	if (!element)
-// 		return (NULL);
-// 	element->env = NULL;
-// 	element->exit_status = &status;
-// 	return (element);
-// }
-
-static void	init_minishell(t_shell *shell, char **env)
+t_shell	*ft_shellnew(char **env)
 {
-	t_env	*lst_env;
+	t_shell	*element;
+	int		status;
 
-	lst_env = call_env(env);
-	// shell = ft_shellnew();
-	shell->env = lst_env;
+	status = 0;
+	element = malloc(sizeof(t_shell));
+	if (!element)
+		return (NULL);
+	element->env = call_env(env);
+	element->exit_status = status;
+	return (element);
 }
+
+/* static void	init_minishell(t_shell *shell, char **env)
+{
+	t_shell	shell;
+} */
 
 static void	exit_free_all(t_token *lst, t_node *node, t_shell *shell, char *buf)
 {
@@ -53,17 +49,17 @@ static void	exit_free_all(t_token *lst, t_node *node, t_shell *shell, char *buf)
 	
 }
 
-static int do_node(t_node *node)
+/* static int do_node(t_node *node)
 {
 	int res;
 
-	res = what_the_buildin(node);
+	res = exec_the_buildin(node);
 	if (res == 1)
 		return (1);
 	if (res == -1)
 		exec_cmd(node, node->cmd->av, rebuild_env(&node->cmd->shell->env));
 	return (0);
-}
+} */
 
 int	main(int ac, char **av, char **env)
 {
@@ -71,34 +67,35 @@ int	main(int ac, char **av, char **env)
 	t_token	*cur;
 	t_token	token;
 	t_node *node;
-	t_shell	shell;
+	t_shell	*shell;
 
 	(void)ac;
 	(void)av;
 	cur = NULL;
 	node = NULL;
-	ft_memset(&shell, 0, (sizeof(t_shell)));
-	init_minishell(&shell, env);
+	shell = ft_shellnew(env);
+	// init_minishell(&shell, env);
 	while (1)
 	{
 		buf = readline("Minishell>");
 		if (buf == NULL)
 		{
 			write(2, "exit\n", 5);
-			exit_free_all(cur, node, &shell, buf);
+			exit_free_all(cur, node, shell, buf);
 		}
 		add_history(buf);
 		cur = lexer(buf, &token);
 		if (cur == NULL)
 			continue ;
-		expand(cur, &shell);
-		node = parse_and_or(&cur, &shell);
+		expand(cur, shell);
+		node = parse_and_or(&cur, shell);
 		if (node)
 			exec_node(node);
-		if (node && node->type == NODE_CMD)
-		{
-			if (do_node(node))
-				exit_free_all(cur, node, &shell, buf);
-		}
+		// printf("exit_status = %i\n", node->cmd->shell->exit_status);
+		// if (node && node->type == NODE_CMD)
+		// {
+		// 	if (do_node(node))
+		// 		exit_free_all(cur, node, shell, buf);
+		// }
 	}
 }
