624ab8e1469cb867eb5ceeade46440b13b509b40
Matthieu Vignes <mvignes@f2r10s19.clusters.42paris.fr>
2026-04-16 14:37:46 +0200

    redirection pour < fonction (merci Noah sans voir mon code). export rajout des '"' pour le contenant des var

--- srcs/execution/redir.c
+++ srcs/execution/redir.c
@@ -6,7 +6,7 @@
 /*   By: mvignes <mvignes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 17:18:23 by mvignes           #+#    #+#             */
-/*   Updated: 2026/04/15 15:57:42 by mvignes          ###   ########.fr       */
+/*   Updated: 2026/04/16 14:25:22 by mvignes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static int	open_file_in(char *outfile)
 	int	fd;
 
 	printf("file = %s\n", outfile);
-	fd = open(outfile, O_WRONLY, 0644);
+	fd = open(outfile, O_RDONLY, 0644);
 	if (fd == -1)
 	{
 		perror("outfile");
