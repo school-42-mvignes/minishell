d6a3146997ceb4bfaf64bb07ea4adfc30bdf9ec0
mvignes <mvignes@f6r9s14.paris.42.school>
2026-04-28 15:56:41 +0200

    . et .. ok

--- srcs/execution/exec_cmd.c
+++ srcs/execution/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mvignes <mvignes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 17:04:41 by mvignes           #+#    #+#             */
-/*   Updated: 2026/04/28 13:56:23 by mvignes          ###   ########.fr       */
+/*   Updated: 2026/04/28 15:54:54 by mvignes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,13 @@ static void	error_exec_cmd(t_shell *shell, char *str, char **env, int exit)
 	ft_putstr_fd("minishell: ", 2);
 	if (str)
 		ft_putstr_fd(str, 2);
-	if (exit == 127)
-		ft_putendl_fd(": command not found", 2);
+	if (!ft_strncmp(str, ".", 2))
+	{
+		ft_putendl_fd(": filename argument required", 2);
+		exit = 2;
+	}
+	else if (exit == 127 || !ft_strncmp(str, "..", 3))
+		(ft_putendl_fd(": command not found", 2), exit = 127);
 	else
 	{
 		if (access(str, X_OK) != 0)
